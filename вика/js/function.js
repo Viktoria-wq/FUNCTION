01e2c4373836a9ebf9366279e73ad1c52f93613f
Viktoria-wq <55383374+Viktoria-wq@users.noreply.github.com>
2019-12-11 16:37:29 +0500

    Add files via upload

---
+++ вика/js/function.js
@@ -0,0 +1,8 @@
+  
+const thirdElement = (string) => {
+	let x = (string[2]);
+	return x;
+}
+
+let result = thirdElement('erhufnjv');
+console.log (result);
---
+++ вика/js/procedure.js
@@ -0,0 +1,7 @@
+  
+const jkThirdElement = (string) => {
+	let x = (string[2]);
+	console.log (x);
+}
+jkThirdElement('fsfsA')
+
